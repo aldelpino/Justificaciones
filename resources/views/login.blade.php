b8a86431922fa1c170cc908e3eb04b4e90da2759
adelpino <alejandro.ignacio.dpa@gmail.com>
2018-09-09 22:16:36 -0230

    Modificacion a Login
    -Se agregó logo IVARAS
    -Se creó css "mi_css"

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -20,37 +20,41 @@
 
     <!-- Custom Theme Style -->
     <link href="../build/css/custom.min.css" rel="stylesheet">
+    <link href="../build/css/mi_css.css" rel="stylesheet">
   </head>
 
   <body class="login">
-    <div>
-      
+    <div class="row">
+      <a class="hiddenanchor" id="signup"></a>
       <a class="hiddenanchor" id="signin"></a>
 
-      <div class="login_wrapper" style="margin-top: 150px">
-        <div class="animate form login_form">
-          <section class="login_content">
-            <form method="post" action="{{ route('login') }}">
-              <input type="hidden" name="_token" value="{{csrf_token()}}">
-              <h1>Sistema de Justificaciones</h1>
-              <div>
-                <input type="email" class="form-control" placeholder="Correo" required="" name="email" value="{{ old('email') }}" />
-                {{ $errors->first('email', ':message') }}
-              </div>
-              <div>
-                <input type="password" class="form-control" placeholder="Contraseña" required="" name="password" />
-                {{ $errors->first('password', '<span class="help-block">:message</span>') }}
-              </div>
-              <div>
-                <button class="btn btn-primary btn-block">Acceder</button>
-                <a class="reset_pass" href="#signup">Perdiste tu contraseña?</a>
-              </div>
+      <div class="container col-md-offset-1">
+        <img src="..\build\images\LOGO_iVARAS.png" class="img-responsive" alt="Cinque Terre" width="304" height="236">
+      </div>
+        <div class="login_wrapper">
+          <div class="animate form login_form">
+            <section class="login_content">
+              <form method="post" action="{{ route('login') }}">
+                <input type="hidden" name="_token" value="{{csrf_token()}}">
+                <h1>Sistema de Justificaciones</h1>
+                <div>
+                  <input type="email" class="form-control" placeholder="Correo" required="" name="email" value="{{ old('email') }}" />
+                  {{ $errors->first('email', ':message') }}
+                </div>
+                <div>
+                  <input type="password" class="form-control" placeholder="Contraseña" required="" name="password" />
+                  {{ $errors->first('password', '<span class="help-block">:message</span>') }}
+                </div>
+                <div>
+                  <button class="btn btn-primary btn-block">Acceder</button>
+                  <a class="reset_pass" href="#signup">Perdiste tu contraseña?</a>
+                </div>
 
-              <div class="clearfix"></div>
+                <div class="clearfix"></div>
 
-            </form>
-          </section>
-        </div>
+              </form>
+            </section>
+          </div>
 
         <div id="register" class="animate form registration_form">
           <section class="login_content">
@@ -71,5 +75,17 @@
         </div>
       </div>
     </div>
+    <div class="row">
+      <footer>
+        <div class="clearfix"></div>
+        <div class="pull-right">
+          Justificaciones Antonio Varas
+        </div>
+      </footer>
+    </div>
+
+
+
+
   </body>
 </html>
