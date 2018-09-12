5bf66382bde92448a3fe52e395fcabb1f60dbcea
Hugo Soto <hsotoorellana@gmail.com>
2018-09-12 08:55:39 -0300

    Add error translations

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'password' => 'Passwords must be at least six characters and match the confirmation.',
+    'reset' => '¡Tu contraseña ha sido reestablecida!',
+    'sent' => '¡Te hemos enviado tu link para reestablecer la contraseña!',
+];
