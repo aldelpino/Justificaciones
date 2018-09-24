2e66b5e504fe19c17f8d126bdbefdd4e6b44d4d8
Hugo Soto <hsotoorellana@gmail.com>
2018-09-24 20:03:10 -0300

    Code clean up

--- app/Http/Controllers/CoordinadorController.php
+++ app/Http/Controllers/CoordinadorController.php
@@ -2,11 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Justification;
-use Log;
-use DB;
-
 
 class CoordinadorController extends Controller
 {
@@ -27,17 +23,13 @@ class CoordinadorController extends Controller
      */
     public function index()
     {
-
-      $listaJustificacionesValidando  = DB::table('justifications')->where([['correo_cor','like', auth()->user()->email],['estado', 'like', 'Pendiente']])->get();
-      $listaJustificacionesAprobadas = DB::table('justifications')->where([['correo_cor','like', auth()->user()->email],['estado', 'like', 'Aprobado']])->limit(1000)->get();
-      $listaJustificacionesRechazadas  = DB::table('justifications')->where([['correo_cor','like', auth()->user()->email],['estado', 'like', 'Rechazado']])->limit(1000)->get();
-      Log::Debug($listaJustificacionesValidando);
-      return view('coordinador/index', [
-        'listaJustificacionesValidando'  => $listaJustificacionesValidando,
-        'listaJustificacionesRechazadas' => $listaJustificacionesRechazadas,
-        'listaJustificacionesAprobadas'  => $listaJustificacionesAprobadas
-      ]);
+        $listaJustificacionesValidando = Justification::where([['correo_cor','like', auth()->user()->email],['estado', 'like', 'Pendiente']])->get();
+        $listaJustificacionesAprobadas = Justification::where([['correo_cor','like', auth()->user()->email],['estado', 'like', 'Aprobado']])->limit(1000)->get();
+        $listaJustificacionesRechazadas = Justification::where([['correo_cor','like', auth()->user()->email],['estado', 'like', 'Rechazado']])->limit(1000)->get();
+        return view('coordinador/index', [
+            'listaJustificacionesValidando'  => $listaJustificacionesValidando,
+            'listaJustificacionesRechazadas' => $listaJustificacionesRechazadas,
+            'listaJustificacionesAprobadas'  => $listaJustificacionesAprobadas
+        ]);
     }
-
-
 }
