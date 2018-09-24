37020c32837f32c1abfcc74c58527c2954110aba
adelpino <alejandro.ignacio.dpa@gmail.com>
2018-09-24 01:23:02 -0300

    Modificacion vista Alumno: Eliminacion de navbar

--- resources/views/alumno/crearJustificacion.blade.php
+++ resources/views/alumno/crearJustificacion.blade.php
@@ -30,16 +30,7 @@
 
         <div class="col-md-12 col-sm-12 col-xs-12">
           <div class="x_panel">
-            <div class="x_title">
-              <ul class="nav navbar-right panel_toolbox">
-                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                </li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                </li>
-              </ul>
-              <div class="clearfix"></div>
-            </div>
+            
             <div class="x_content">
 
 
