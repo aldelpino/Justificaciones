0da5198a9f4e69eff48f35ee422165152f2a861e
Hugo Soto <hsotoorellana@gmail.com>
2018-09-25 23:38:53 -0300

    Tweaks on approved mails

--- app/Listeners/Justification/Approved/SendEmailToTeacher.php
+++ app/Listeners/Justification/Approved/SendEmailToTeacher.php
@@ -5,6 +5,7 @@ namespace App\Listeners\Justification\Approved;
 use App\Events\Justification\Approved as JustificationApproved;
 use App\Mail\Justification\Approved\ToTeacher as JustificationApprovedEmail;
 use Mail;
+use DB;
 
 class SendEmailToTeacher
 {
@@ -26,6 +27,10 @@ class SendEmailToTeacher
      */
     public function handle(JustificationApproved $event)
     {
-        Mail::to($event->teacherEmail)->send(new JustificationApprovedEmail());
+        $alumno = DB::table('datos_semestre')
+            ->where('CORREO_ALUM', $event->studentEmail)
+            ->first(['rut_alu', 'carrera', 'NOMBRE_ALUM', 'APEP_ALUM', 'NOMBRE_DOC', 'APEP_DOC', 'NOMBRE_COR', 'APEP_COR']);
+        Mail::to($event->teacherEmail)
+            ->send(new JustificationApprovedEmail($event->justification, $alumno));
     }
 }
