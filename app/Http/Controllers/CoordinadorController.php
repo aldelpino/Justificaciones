3dfda129d6bbe29299c0da2ee09db5ef425c0dc3
Hugo Soto <hsotoorellana@gmail.com>
2018-09-25 23:53:35 -0300

    Minor corrections on coordinator

--- app/Http/Controllers/CoordinadorController.php
+++ app/Http/Controllers/CoordinadorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Justification;
+use DB;
 
 class CoordinadorController extends Controller
 {
@@ -23,30 +24,31 @@ class CoordinadorController extends Controller
      */
     public function index()
     {
+        $listaJustificacionesValidando = DB::table('justifications')
+            ->select('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
+            ->join('datos_semestre', 'justifications.correo_alum', 'datos_semestre.correo_alum')
+            ->where([['justifications.correo_cor','like', auth()->user()->email],['estado', 'like', 'Pendiente']])
+            ->groupBy('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
+            ->get();
 
-      $listaJustificacionesValidando  = DB::table('justifications')
-                                        ->select('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
-                                        ->join('datos_semestre', 'justifications.correo_alum', 'datos_semestre.correo_alum')
-                                        ->where([['justifications.correo_cor','like', auth()->user()->email],['estado', 'like', 'Pendiente']])
-                                        ->groupBy('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
-                                        ->get();
-      $listaJustificacionesAprobadas = DB::table('justifications')
-                                        ->select('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
-                                        ->join('datos_semestre', 'justifications.correo_alum', 'datos_semestre.correo_alum')
-                                        ->where([['justifications.correo_cor','like', auth()->user()->email],['estado', 'like', 'Aprobado']])
-                                        ->groupBy('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
-                                        ->limit(1000)->get();
-      $listaJustificacionesRechazadas  =DB::table('justifications')
-                                        ->select('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
-                                        ->join('datos_semestre', 'justifications.correo_alum', 'datos_semestre.correo_alum')
-                                        ->where([['justifications.correo_cor','like', auth()->user()->email],['estado', 'like', 'Rechazado']])
-                                        ->groupBy('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
-                                        ->limit(1000)->get();
-      Log::Debug($listaJustificacionesValidando);
-      return view('coordinador/index', [
-        'listaJustificacionesValidando'  => $listaJustificacionesValidando,
-        'listaJustificacionesRechazadas' => $listaJustificacionesRechazadas,
-        'listaJustificacionesAprobadas'  => $listaJustificacionesAprobadas
-      ]);
+        $listaJustificacionesAprobadas = DB::table('justifications')
+            ->select('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
+            ->join('datos_semestre', 'justifications.correo_alum', 'datos_semestre.correo_alum')
+            ->where([['justifications.correo_cor','like', auth()->user()->email],['estado', 'like', 'Aprobado']])
+            ->groupBy('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
+            ->limit(1000)->get();
+
+        $listaJustificacionesRechazadas = DB::table('justifications')
+            ->select('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
+            ->join('datos_semestre', 'justifications.correo_alum', 'datos_semestre.correo_alum')
+            ->where([['justifications.correo_cor','like', auth()->user()->email],['estado', 'like', 'Rechazado']])
+            ->groupBy('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
+            ->limit(1000)->get();
+        logger($listaJustificacionesValidando);
+        return view('coordinador/index', [
+            'listaJustificacionesValidando' => $listaJustificacionesValidando,
+            'listaJustificacionesRechazadas' => $listaJustificacionesRechazadas,
+            'listaJustificacionesAprobadas' => $listaJustificacionesAprobadas
+        ]);
     }
 }
