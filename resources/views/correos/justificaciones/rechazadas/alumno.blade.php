ab9d6890e5155e8ddb62f01b84edf663751c83d3
Hugo Soto <hsotoorellana@gmail.com>
2018-09-28 18:33:59 -0300

    Modify mail views for mailables

--- resources/views/correos/justificaciones/rechazadas/alumno.blade.php
+++ resources/views/correos/justificaciones/rechazadas/alumno.blade.php
@@ -3,14 +3,21 @@
 
 Le informamos que la solicitud <strong>{{ $folio }}</strong> ha sido <strong>RECHAZADA</strong> por su coordinador(a)
 con fecha <strong>{{ date("d-m-Y", strtotime($fechaJustificacion)) }}</strong>.
-Su docente <strong>{{ $nombreProfesor }}</strong> será informado de esta situación.
+Su docente será informado de esta situación.
 
 ## Detalle Solicitud
 
 * Rut: {{ $rutAlumno }}
 * Nombre: {{ $nombreAlumno }}
 * Carrera: {{ $carreraAlumno }}
-* Asignatura: {{ $asignatura }}
+* Docentes:
+@foreach($nombreProfesores as $p)
+  * {{ $p }}
+@endforeach
+* Asignaturas:
+@foreach ($asignaturas as $a)
+  * {{ $a }}
+@endforeach
 * Resolución: {{ $resolucion }}
 
 Favor, conservar este mail.
