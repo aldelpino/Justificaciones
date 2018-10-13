929c0c885b54e9d9f6044f3bd700a7bac40883ea
adelpino <alejandro.ignacio.dpa@gmail.com>
2018-10-12 21:13:54 -0300

    Modificacion Alumno: Correcion Datos Columnas vistas Index y RevisarJustificaciones

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="images/favicon.ico" type="image/ico" />
 
-    <title>Duoc Uc - Justificaciones</title>
+    <title>Justificaciones - Antonio Varas</title>
 
     <!-- Bootstrap -->
     <link href="/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
--- resources/views/layouts/alumno.blade.php
+++ resources/views/layouts/alumno.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/ico" />
-    <title>Duoc Uc - Justificaciones</title>
+    <title>Justificaciones - Antonio Varas</title>
 
     <!-- Bootstrap -->
     <link href="/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
